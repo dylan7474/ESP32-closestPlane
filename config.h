8bfd0ad01d6822db6a814a7851a464487588f914
dylan7474 <112658365+dylan7474@users.noreply.github.com>
2025-08-30 23:51:13 +0100

    Update config.h

--- config.h
+++ config.h
@@ -5,15 +5,18 @@
 #define WIFI_SSID "YOUR_WIFI_SSID"
 #define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
 
-// ADS-B dump1090 server configuration
-#define DUMP1090_SERVER "127.0.0.1" // IP or hostname of dump1090 server
-#define DUMP1090_PORT 8080          // Port of dump1090 JSON endpoint
-#define USER_LAT 51.5074
-#define USER_LON -0.1278
+// -- User Location --
+#define USER_LAT 54.5 
+#define USER_LON -1.0
+
+// -- Radar & Server Settings --
+#define RADAR_RANGE_KM 50.0             // NEW: Only track aircraft within this distance (in km)
+#define DUMP1090_SERVER "192.168.50.100" // IP or hostname of dump1090 server
+#define DUMP1090_PORT 8080               // The default web port for dump1090 is often 8080
 
 // I2S pins for MAX98357A audio output
-#define I2S_BCLK_PIN 26
-#define I2S_LRCLK_PIN 25
-#define I2S_DOUT_PIN 22
+#define I2S_BCLK_PIN  17
+#define I2S_LRCLK_PIN 16
+#define I2S_DOUT_PIN  27
 
 #endif // CONFIG_H
